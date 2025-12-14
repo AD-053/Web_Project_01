d3cd0d6cd0276045c54dee640d195141c5d42091
Autanu Datta <autanu2025@gmail.com>
2025-12-15 01:04:23 +0600

    Add email parameter to register method

--- src/main/java/com/rahad/dao/register.java
+++ src/main/java/com/rahad/dao/register.java
@@ -3,7 +3,7 @@ package com.rahad.dao;
 import java.sql.*;
 
 public class register {
-    public boolean set(String fullname, String username, String password, String gender, String role) throws ClassNotFoundException, SQLException {
+    public boolean set(String fullname, String username, String password, String gender, String role, String email) throws ClassNotFoundException, SQLException {
      
         Class.forName("com.mysql.cj.jdbc.Driver");
 
@@ -12,7 +12,7 @@ public class register {
         try (Connection con = DriverManager.getConnection(url, "root", "sust53")) {
 
        
-            String insert = "INSERT INTO signup_data (name, username, password, gender, role) VALUES (?, ?, ?, ?, ?)";
+            String insert = "INSERT INTO signup_data (name, username, password, gender, role, email) VALUES (?, ?, ?, ?, ?, ?)";
 
             try (PreparedStatement stm = con.prepareStatement(insert)) {
                 stm.setString(1, fullname);
@@ -20,6 +20,7 @@ public class register {
                 stm.setString(3, password);
                 stm.setString(4, gender);
                 stm.setString(5, role);
+                stm.setString(6, email);
 
                 int rowsInserted = stm.executeUpdate();
                 return rowsInserted > 0; 
